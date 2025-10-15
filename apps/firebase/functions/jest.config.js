79c626c9a55636358dc04d3a247108a66bb90447
Josiah Saunders <contact@josiahsaunders.com>
2025-10-15 08:33:11 -0600

    test wip

--- apps/firebase/functions/jest.config.js
+++ apps/firebase/functions/jest.config.js
@@ -1,6 +1,16 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-	preset: "ts-jest",
+	preset: "ts-jest/presets/default-esm",
 	testEnvironment: "node",
 	testTimeout: 10000,
+	extensionsToTreatAsEsm: [".ts"],
+	moduleNameMapper: {
+		"^(\\.{1,2}/.*)\\.js$": "$1",
+	},
+	globals: {
+		"ts-jest": {
+			useESM: true,
+			tsconfig: "./tsconfig.test.json",
+		},
+	},
 };
