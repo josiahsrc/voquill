cdbf69f73240e590d449b3ae0b801d07c499cce4
Josiah Saunders <contact@josiahsaunders.com>
2025-10-28 06:21:15 -0600

    try fix redirect

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 export default defineConfig({
+  site: "https://voquill.com",
   output: "static",
   integrations: [react()],
 });
