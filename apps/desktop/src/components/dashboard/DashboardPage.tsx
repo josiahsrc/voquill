0691e995e8a15731d7ef21fd34de52c560b1da78
Josiah Saunders <contact@josiahsaunders.com>
2025-10-30 22:47:54 -0600

    show the version

--- apps/desktop/src/components/dashboard/DashboardPage.tsx
+++ apps/desktop/src/components/dashboard/DashboardPage.tsx
@@ -1,8 +1,12 @@
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
+import { getVersion } from "@tauri-apps/api/app";
 import { Outlet } from "react-router-dom";
+import { useAsyncData } from "../../hooks/async.hooks";
 import { DashboardMenu } from "./DashboardMenu";
 
 export default function DashboardPage() {
+  const data = useAsyncData(getVersion, []);
+
   return (
     <Stack direction="row" sx={{ height: "100%", width: "100%" }}>
       <Box
@@ -15,6 +19,19 @@ export default function DashboardPage() {
         <DashboardMenu />
       </Box>
       <Outlet />
+      <Typography
+        variant="caption"
+        sx={{
+          position: "fixed",
+          bottom: 2,
+          left: 8,
+          fontSize: "0.65rem",
+          color: "text.secondary",
+          opacity: 0.2,
+        }}
+      >
+        {data.state === "success" ? `v${data.data}` : ""}
+      </Typography>
     </Stack>
   );
 }
