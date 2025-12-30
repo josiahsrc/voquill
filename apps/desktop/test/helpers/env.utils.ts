0c40781b9b95f362693c9be3b5d48adc72f20153
Josiah Saunders <contact@josiahsaunders.com>
2025-12-30 10:05:21 -0700

    remove dummy integration test

--- apps/desktop/test/helpers/env.utils.ts
+++ apps/desktop/test/helpers/env.utils.ts
@@ -6,10 +6,6 @@ function getEnvOrThrow(key: string): string {
   return value;
 }
 
-export function getTestApiKey(): string {
-  return getEnvOrThrow("TEST_API_KEY");
-}
-
 export function getGroqApiKey(): string {
   return getEnvOrThrow("GROQ_API_KEY");
 }
