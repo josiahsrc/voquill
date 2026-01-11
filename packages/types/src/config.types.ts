9cefdaf33824e805926be86292909971b513f0a4
Josiah Saunders <contact@josiahsaunders.com>
2026-01-11 07:43:59 -0700

    adjust token limits to be more realistic

--- packages/types/src/config.types.ts
+++ packages/types/src/config.types.ts
@@ -9,18 +9,20 @@ export type FullConfig = {
   proTokensPerMonth: number;
 };
 
+const TOKEN_MULT = 18;
+const MONTH_MULT = 30;
 const FREE_WORDS_PER_DAY = 1_000;
-const FREE_TOKENS_PER_DAY = FREE_WORDS_PER_DAY * 100;
-const PRO_WORDS_PER_DAY = 50_000;
-const PRO_TOKENS_PER_DAY = PRO_WORDS_PER_DAY * 100;
+const FREE_TOKENS_PER_DAY = FREE_WORDS_PER_DAY * TOKEN_MULT;
+const PRO_WORDS_PER_DAY = 15_000;
+const PRO_TOKENS_PER_DAY = PRO_WORDS_PER_DAY * TOKEN_MULT;
 
 export const FULL_CONFIG: FullConfig = {
   freeWordsPerDay: FREE_WORDS_PER_DAY,
-  freeWordsPerMonth: FREE_WORDS_PER_DAY * 20,
+  freeWordsPerMonth: FREE_WORDS_PER_DAY * MONTH_MULT,
   freeTokensPerDay: FREE_TOKENS_PER_DAY,
-  freeTokensPerMonth: FREE_TOKENS_PER_DAY * 20,
+  freeTokensPerMonth: FREE_TOKENS_PER_DAY * MONTH_MULT,
   proWordsPerDay: PRO_WORDS_PER_DAY,
-  proWordsPerMonth: PRO_WORDS_PER_DAY * 20,
+  proWordsPerMonth: PRO_WORDS_PER_DAY * MONTH_MULT,
   proTokensPerDay: PRO_TOKENS_PER_DAY,
-  proTokensPerMonth: PRO_TOKENS_PER_DAY * 20,
+  proTokensPerMonth: PRO_TOKENS_PER_DAY * MONTH_MULT,
 };
