8f9e0e2839196ea31eabe7e46e86598ecf6c72bc
Josiah Saunders <contact@josiahsaunders.com>
2026-01-18 09:26:00 -0700

    Revert "load vulkan dynamically"
    This reverts commit 8566ac4810738224da632b7d595108326dc89402.

--- apps/desktop/src-tauri/build.rs
+++ apps/desktop/src-tauri/build.rs
@@ -1,9 +1,3 @@
 fn main() {
-    #[cfg(all(target_os = "windows", feature = "windows-gpu"))]
-    {
-        println!("cargo:rustc-link-arg=/DELAYLOAD:vulkan-1.dll");
-        println!("cargo:rustc-link-lib=delayimp");
-    }
-
     tauri_build::build()
 }
