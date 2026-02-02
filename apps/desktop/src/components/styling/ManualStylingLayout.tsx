e2639c5403329647a219a34f414e23b73b5392dc
Josiah Saunders <contact@josiahsaunders.com>
2026-02-02 07:30:15 -0700

    add helpful subtitle

--- apps/desktop/src/components/styling/ManualStylingLayout.tsx
+++ apps/desktop/src/components/styling/ManualStylingLayout.tsx
@@ -1,12 +1,58 @@
-import { Stack, Typography } from "@mui/material";
+import { Link, Stack, Typography } from "@mui/material";
 import { FormattedMessage } from "react-intl";
 import { loadTones } from "../../actions/tone.actions";
 import { useAsyncEffect } from "../../hooks/async.hooks";
-import { useAppStore } from "../../store";
+import { produceAppState, useAppStore } from "../../store";
+import {
+  getHotkeyCombosForAction,
+  SWITCH_WRITING_STYLE_HOTKEY,
+} from "../../utils/keyboard.utils";
 import { getActiveManualToneIds } from "../../utils/tone.utils";
+import { HotkeyBadge } from "../common/HotkeyBadge";
 import { VirtualizedListPage } from "../common/VirtualizedListPage";
 import { ManualAddStyle } from "./ManualAddStyle";
 import { ManualStylingRow } from "./ManualStylingRow";
+
+function StylingSubtitle() {
+  const combos = useAppStore((state) =>
+    getHotkeyCombosForAction(state, SWITCH_WRITING_STYLE_HOTKEY),
+  );
+
+  const openShortcuts = () =>
+    produceAppState((draft) => {
+      draft.settings.shortcutsDialogOpen = true;
+    });
+
+  if (combos.length === 0) {
+    return (
+      <FormattedMessage
+        defaultMessage="Choose different writing styles to change how you sound. You can also <link>set up a hotkey</link> to switch between them faster."
+        values={{
+          link: (chunks: React.ReactNode) => (
+            <Link
+              component="button"
+              sx={{ verticalAlign: "baseline", fontSize: "inherit" }}
+              onClick={openShortcuts}
+            >
+              {chunks}
+            </Link>
+          ),
+        }}
+      />
+    );
+  }
+
+  const hotkey = (
+    <HotkeyBadge keys={combos[0]} onClick={openShortcuts} sx={{ mx: 0.25 }} />
+  );
+
+  return (
+    <FormattedMessage
+      defaultMessage="Choose different writing styles to change how you sound. Switch between them using the {hotkey} hotkey."
+      values={{ hotkey }}
+    />
+  );
+}
 
 export function ManualStylingLayout() {
   useAsyncEffect(async () => {
@@ -18,9 +64,8 @@ export function ManualStylingLayout() {
   return (
     <VirtualizedListPage
       title={<FormattedMessage defaultMessage="Writing Styles" />}
-      subtitle={
-        <FormattedMessage defaultMessage="Choose different writing styles to change how you sound." />
-      }
+      heightMult={7}
+      subtitle={<StylingSubtitle />}
       action={<ManualAddStyle />}
       items={toneIds}
       computeItemKey={(id) => id}
