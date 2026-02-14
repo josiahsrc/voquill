5b4b41df20a5b25ba5a8d3ea2e979b04a2654dcb
Josiah Saunders <contact@josiahsaunders.com>
2026-02-14 09:10:43 -0700

    transcription tile rendering

--- mobile/lib/widgets/history/transcription_tile.dart
+++ mobile/lib/widgets/history/transcription_tile.dart
@@ -1,10 +1,12 @@
+import 'dart:async';
+
 import 'package:app/store/store.dart';
+import 'package:app/utils/date_utils.dart';
 import 'package:app/widgets/common/app_list_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:intl/intl.dart';
 
-class TranscriptionTile extends StatelessWidget {
+class TranscriptionTile extends StatefulWidget {
   const TranscriptionTile({
     super.key,
     required this.id,
@@ -15,21 +17,41 @@ class TranscriptionTile extends StatelessWidget {
   final VoidCallback? onTap;
 
   @override
+  State<TranscriptionTile> createState() => _TranscriptionTileState();
+}
+
+class _TranscriptionTileState extends State<TranscriptionTile> {
+  late final Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(
+      const Duration(minutes: 1),
+      (_) => setState(() {}),
+    );
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final transcription =
-        useAppStore().select(context, (s) => s.transcriptionById[id]);
+        useAppStore().select(context, (s) => s.transcriptionById[widget.id]);
     if (transcription == null) return const SizedBox.shrink();
 
     return AppListTile(
-      onTap: onTap,
+      onTap: widget.onTap,
       title: Text(
-        transcription.text,
+        transcription.text.replaceAll(RegExp(r'\s*\n\s*'), ' '),
         maxLines: 2,
         overflow: TextOverflow.ellipsis,
       ),
-      subtitle: Text(
-        DateFormat.yMMMd().add_jm().format(transcription.createdAtDate),
-      ),
+      subtitle: Text(formatRelativeDate(transcription.createdAtDate)),
       trailing: IconButton(
         icon: const Icon(Icons.copy, size: 20),
         onPressed: () {
