5a82758ce970298ce2b2c390584bffca3b483d4c
Josiah Saunders <contact@josiahsaunders.com>
2026-02-15 17:37:42 -0700

    remove outro page because not needed

--- mobile/lib/widgets/setup/setup_keyboard_form.dart
+++ mobile/lib/widgets/setup/setup_keyboard_form.dart
@@ -1,19 +1,17 @@
 import 'package:app/widgets/common/multi_page_presenter.dart';
 import 'package:app/widgets/permissions/keyboard_permissions.dart';
-import 'package:app/widgets/setup/setup_outro_form.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SetupKeyboardForm extends StatelessWidget {
   const SetupKeyboardForm({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final presenter = context.presenter();
-
     return KeyboardPermissions(
       backButton: const MultiPageBackButton(),
       nextButton: FilledButton(
-        onPressed: () => presenter.pushPage<SetupOutroForm>(),
+        onPressed: () => context.go('/dashboard'),
         child: const Text('Next'),
       ),
     );
--- mobile/lib/widgets/setup/setup_page.dart
+++ mobile/lib/widgets/setup/setup_page.dart
@@ -2,7 +2,6 @@ import 'package:app/widgets/common/multi_page_presenter.dart';
 import 'package:app/widgets/setup/setup_intro_form.dart';
 import 'package:app/widgets/setup/setup_keyboard_form.dart';
 import 'package:app/widgets/setup/setup_microphone_form.dart';
-import 'package:app/widgets/setup/setup_outro_form.dart';
 import 'package:flutter/material.dart';
 
 class SetupPage extends StatefulWidget {
@@ -30,7 +29,6 @@ class _SetupPageState extends State<SetupPage> {
           MultiPageItem.fromPage(const SetupIntroForm()),
           MultiPageItem.fromPage(const SetupMicrophoneForm()),
           MultiPageItem.fromPage(const SetupKeyboardForm()),
-          MultiPageItem.fromPage(const SetupOutroForm()),
         ],
       ),
     );
