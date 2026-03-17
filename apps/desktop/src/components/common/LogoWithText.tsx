8c49f9979a120908dcb4e3136817ef79ca657918
Josiah Saunders <contact@josiahsaunders.com>
2026-03-17 15:41:36 -0600

    move chips out of stateless component

--- apps/desktop/src/components/common/LogoWithText.tsx
+++ apps/desktop/src/components/common/LogoWithText.tsx
@@ -1,23 +1,9 @@
-import { ArrowOutwardRounded, SouthWestRounded } from "@mui/icons-material";
-import { Chip, Stack, Tooltip, Typography, type StackProps } from "@mui/material";
-import { useIntl } from "react-intl";
-import { useAppStore } from "../../store";
-import { getActiveRemoteTarget, getRemoteReceiverStatus } from "../../utils/device.utils";
-import { getMyUserPreferences } from "../../utils/user.utils";
+import { Stack, Typography, type StackProps } from "@mui/material";
 import { Logo } from "./Logo";
 
 export type LogoWithTextProps = StackProps;
 
 export const LogoWithText = ({ sx, ...rest }: LogoWithTextProps) => {
-  const intl = useIntl();
-  const [activeRemoteTarget, remoteReceiverEnabled] = useAppStore((state) => [
-    getActiveRemoteTarget(state),
-    getRemoteReceiverStatus(state)?.enabled ?? false,
-  ]);
-  const remoteOutputEnabled = useAppStore(
-    (state) => getMyUserPreferences(state)?.remoteOutputEnabled ?? false,
-  );
-
   return (
     <Stack
       direction="row"
@@ -40,39 +26,6 @@ export const LogoWithText = ({ sx, ...rest }: LogoWithTextProps) => {
       >
         Voquill
       </Typography>
-      <Stack direction="row" spacing={0.5} sx={{ ml: 1 }}>
-        {remoteOutputEnabled && activeRemoteTarget && (
-          <Tooltip
-            title={intl.formatMessage(
-              { defaultMessage: "Sending to {name}" },
-              { name: activeRemoteTarget.name },
-            )}
-          >
-            <Chip
-              size="small"
-              variant="outlined"
-              icon={<ArrowOutwardRounded />}
-              label={intl.formatMessage({ defaultMessage: "Sender" })}
-              sx={{ height: 22, display: { xs: "none", sm: "inline-flex" } }}
-            />
-          </Tooltip>
-        )}
-        {remoteReceiverEnabled && (
-          <Tooltip
-            title={intl.formatMessage({
-              defaultMessage: "Receiver is listening for remote transcripts",
-            })}
-          >
-            <Chip
-              size="small"
-              variant="outlined"
-              icon={<SouthWestRounded />}
-              label={intl.formatMessage({ defaultMessage: "Receiver" })}
-              sx={{ height: 22, display: { xs: "none", sm: "inline-flex" } }}
-            />
-          </Tooltip>
-        )}
-      </Stack>
     </Stack>
   );
 };
