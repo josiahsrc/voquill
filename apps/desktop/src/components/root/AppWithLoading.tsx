3cb6381b32f4e80dffd69113be1f0113e5b65bb6
Josiah Saunders <contact@josiahsaunders.com>
2026-03-18 15:50:03 -0600

    linux in-app hotkey recording

--- apps/desktop/src/components/root/AppWithLoading.tsx
+++ apps/desktop/src/components/root/AppWithLoading.tsx
@@ -1,8 +1,10 @@
 import { Box } from "@mui/material";
 import Router from "../../router";
 import { useAppStore } from "../../store";
+import { getPlatform } from "../../utils/platform.utils";
 import { AppSideEffects } from "./AppSideEffects";
 import { DictationSideEffects } from "./DictationSideEffects";
+import { KeyPressSideEffects } from "./KeyPressSideEffects";
 import { LoadingApp } from "./LoadingApp";
 import { UpdateDialog } from "./UpdateDialog";
 
@@ -11,6 +13,9 @@ export const AppWithLoading = () => {
 
   return (
     <>
+      {/* Linux uses compositor for hotkeys, so we emulate key-pressed here */}
+      {getPlatform() === "linux" && <KeyPressSideEffects />}
+
       <AppSideEffects />
       <UpdateDialog />
       <DictationSideEffects />
