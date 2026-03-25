f433a5acabd6bc79be713f0f980dd7e09fb1d6ec
Josiah Saunders <contact@josiahsaunders.com>
2026-03-25 15:41:12 -0600

    always use trailing slash

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 import { locales, defaultLocale } from './src/types/locale';
 
 export default defineConfig({
+	site: 'https://voquill.com',
+	trailingSlash: 'always',
 	i18n: {
 		defaultLocale,
 		locales: [...locales],
