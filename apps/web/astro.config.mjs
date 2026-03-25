de8fae32610e9c3d9c2a96ceb4109507e1a04873
Josiah Saunders <contact@josiahsaunders.com>
2026-03-25 10:11:49 -0600

    language redirection

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { locales, defaultLocale } from './src/types/locale';
 
 export default defineConfig({
 	i18n: {
-		defaultLocale: 'en',
-		locales: ['en', 'es'],
+		defaultLocale,
+		locales: [...locales],
 		routing: {
 			prefixDefaultLocale: true,
 		},
