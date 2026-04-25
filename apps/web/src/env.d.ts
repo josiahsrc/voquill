b61ae0b32d80cd12b694e872247bc84abf60bc19
Josiah Saunders <contact@josiahsaunders.com>
2026-04-24 18:20:20 -0700

    clean up web

--- apps/web/src/env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="astro/client" />
-
-interface ImportMetaEnv {
-	readonly PUBLIC_MIXPANEL_TOKEN: string;
-}
-
-interface ImportMeta {
-	readonly env: ImportMetaEnv;
-}
--- apps/web/src/utils/locale.utils.ts
+++
@@ -1,7 +0,0 @@
-import { locales } from '../types/locale';
-
-const localePattern = new RegExp(`^/(${locales.join('|')})`);
-
-export function stripLocalePrefix(pathname: string): string {
-	return pathname.replace(localePattern, '');
-}
